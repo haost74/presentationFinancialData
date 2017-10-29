d889cd3a748e062247991576a55a90e6a4ba9f34
Алексей <haost@yandex.ru>
2017-10-29 19:32:34 +0300

    запрос к сервису wcf и возвращение результата в json

--- start.js
+++ start.js
@@ -40,12 +40,30 @@ app.post("/user", jsonParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
    var userName = os.userInfo().username;
-  var rt = JSON.stringify(cbrfdata.getMessage("1", parseString));
+   var result = "";
+  //var rt = JSON.stringify(cbrfdata.getMessage("1", parseString));
+  var BasicHttpBinding =  require('wcf.js').BasicHttpBinding
+    , Proxy = require('wcf.js').Proxy
+    , binding = new BasicHttpBinding()
+    , proxy = new Proxy(binding, "http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx")
+    , message = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/"><Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><mrrf xmlns="http://web.cbr.ru/"><fromDate>2017-01-01T00:00:00</fromDate><ToDate>2017-12-31T00:00:00</ToDate></mrrf></Body></Envelope>'
 
-   response.send(rt);//request.body);
+var rex = response;
+
+     proxy.send(message, "http://web.cbr.ru/mrrf", function(response, ctx) {
+              var result =soap(response, rex);
+              console.log(result);
+              });
 });
 
+function soap(response, rex){
+  var rt;
+parseString(response, function(err, result){
 
+   rt = JSON.stringify(result['soap:Envelope']['soap:Body']);
+  //console.log(rt);
+  rex.send(result);
+});};
 
 
 app.get('/', function(req, res) {
