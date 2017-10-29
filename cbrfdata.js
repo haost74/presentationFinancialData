151953afa8ee39d7d3341aed674acc233bfe9a46
Алексей <haost@yandex.ru>
2017-10-29 10:02:14 +0300

    obtaining data on mrrf of wcf web.cbr.ru

--- cbrfdata.js
+++ cbrfdata.js
@@ -1,7 +1,24 @@
-var currentDate = new Date();
-module.exports.date = currentDate;
+var parseString = require('xml2js').parseString;
 
 module.exports.getMessage = function(flag, data){
 
-return data;
+  var BasicHttpBinding = require('wcf.js').BasicHttpBinding
+    , Proxy = require('wcf.js').Proxy
+    , binding = new BasicHttpBinding()
+    , proxy = new Proxy(binding, "http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx")
+    , message = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/"><Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><mrrf xmlns="http://web.cbr.ru/"><fromDate>2017-01-01T00:00:00</fromDate><ToDate>2017-12-31T00:00:00</ToDate></mrrf></Body></Envelope>'
+
+    proxy.send(message, "http://web.cbr.ru/mrrf", function(response, ctx) {
+
+                soap(response, parseString);
+              });
+}
+
+function soap(response){
+
+ parseString(response, function(err, result){
+   var rt = JSON.stringify(result['soap:Envelope']['soap:Body']);
+   console.dir(rt);
+ });
+
 }
