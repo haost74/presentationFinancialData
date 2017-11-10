7c91baa163421f570684ef65b41bc3b674b78fbc
Алексей <haost@yandex.ru>
2017-11-10 21:19:15 +0300

    получение строки mmfr

--- cbrfdata.js
+++ cbrfdata.js
@@ -7,13 +7,13 @@ module.exports.getMessage = function(data){
 
   for(var i = 0; i < data.length; ++i){
     //serData(data[i]);
-    console.log(obj[i]["D0"]);
-    console.log(obj[i]["p1"]);
-    console.log(obj[i]["p2"]);
-    console.log(obj[i]["p3"]);
-    console.log(obj[i]["p4"]);
-    console.log(obj[i]["p5"]);
-    console.log(obj[i]["p6"]);
+    console.log(data[i]["D0"][0]);
+    console.log(data[i]["p1"][0]);
+    console.log(data[i]["p2"][0]);
+    console.log(data[i]["p3"][0]);
+    console.log(data[i]["p4"][0]);
+    console.log(data[i]["p5"][0]);
+    console.log(data[i]["p6"][0]);
     return;
   }
 };
